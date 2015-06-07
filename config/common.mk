3f0dc9e8794280ca5bfe817794b4689ecd6d3f45
noname55804 <marlin2201@gmail.com>
2015-06-07 15:50:07 +0200

    common: remove CmAccount and SetupWizard
    and comment out CMUpdater
    
    Change-Id: I4198d596d152d43ece5320105c79befacb3b6fbc
    Signed-off-by: noname55804 <marlin2201@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -44,7 +44,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
@@ -122,17 +121,19 @@ PRODUCT_PACKAGES += \
 
 # Custom CM packages
 PRODUCT_PACKAGES += \
-    Launcher3 \
-    Trebuchet \
     AudioFX \
     PXWallpapers \
     CMFileManager \
     Eleven \
-    LockClock \
-    CMUpdater \
-    CMAccount \
-    CMHome \
-    CyanogenSetupWizard
+    LockClock 
+
+#    CMUpdater
+
+# Launcher packages
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    Trebuchet \
+    CMHome
 
 # CM Platform Library
 PRODUCT_PACKAGES += \
